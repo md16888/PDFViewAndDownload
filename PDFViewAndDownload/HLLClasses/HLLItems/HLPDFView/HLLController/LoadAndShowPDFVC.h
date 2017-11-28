4ff39876854c80b50db6a1bb7163f429c22dcbe1
md16888 <175095363@qq.com>
2017-11-28 11:19:33 +0800

    Rename LoadAndShowPDFVC to LoadAndShowPDFVC.h

---
+++ PDFViewAndDownload/HLLClasses/HLLItems/HLPDFView/HLLController/LoadAndShowPDFVC.h
@@ -0,0 +1,15 @@
+//
+//  LoadAndShowPDFVC.h
+//  PDFViewAndDownload
+//
+//  Created by modong on 2017/11/27.
+//  Copyright © 2017年 Jeffrey hu. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface LoadAndShowPDFVC : BaseViewController
+
+@property (nonatomic, copy)     NSString *webUrlString;
+
+@end
